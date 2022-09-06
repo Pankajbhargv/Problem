ddac4f76d72324b6850a5f417a5393410e931161
Anjul Gupta <76518288+AnjulGupta12@users.noreply.github.com>
2022-09-06 17:01:24 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -3,6 +3,10 @@ vector<int> frequencySort(vector<int>& nums) {
         //key: element
         //value: freq -> compare
         
+//         lets take eq of 
+//         nums = [1,1,2,2,2,3]
+//         output = [3,1,1,2,2,2]
+        
         int freq;
         
         for(int i=0; i<nums.size(); i++)
@@ -19,17 +23,25 @@ vector<int> frequencySort(vector<int>& nums) {
                 m.find(nums[i])->second=freq;     //fixed element with frquency
             }
         }
+        
+//         m map: key   value 
+//                 1       2
+//                 2       3
+//                 3       1
+        
         vector<int> v;
          for(int i=1; i<m.size()+1; i++)
          {
              int value =  m.find(m[i])->second;
              v.push_back(value);                // pushed frequency in a vector
+                                                // v = 2,3,1 these are the value element of map
          }
         sort(v.begin(),v.end(),[](int a,int b){
             if(a>b)
             return true;
             else
             return false;                     //arrange freq in dec order
+                                             // v= 3,2,1
         });
         
         //now i want to print key in number of time freq
@@ -46,7 +58,7 @@ vector<int> frequencySort(vector<int>& nums) {
                 }
             }
         }
-           return ge;  
+           return ge;                          // here I ruined (inc time complexity)                                                                         
         }
      
        
